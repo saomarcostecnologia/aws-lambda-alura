95b36a38c50500e1a99fb9d226c6d7bd2a6faa7a
saomarcostecnologia <saomarcostecnologia@gmail.com>
2024-08-22 20:56:41 -0300

    bucando obj do s3

--- index.mjs
+++ index.mjs
@@ -1,10 +1,24 @@
 import { log } from "./log.mjs";
+import { S3 } from '@aws-sdk/client-s3';   
 
-export const handler = async (event) => {
-  log('Log de execução, Após github action event:' + JSON.stringify(event));
+const s3Client = new S3({region: 'us-east-1'});
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
+export const handler = async (event, context, callback) => {
+    const record = event.record[0];
+    const Bucket = record.s3.bucket.name;
+    const Key = record.s3.object.key;
+    const getObjectResult =  s3Client.getObject({
+        Bucket,
+        Key,
+    }).then( getObjectResult => {
+        const mega_byte = 1024 * 1024;
+
+        if (getObjectResult.ContentLength > 1 * mega_byte) {
+          log('Objeto muito grande');
+          callback('Objeto muito grande');
+        }
+    
+        log('Objeto de Tamanho OK');
+        callback('Objeto de Tamanho OK');
+    });
 };
