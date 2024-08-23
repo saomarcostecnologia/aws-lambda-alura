22da45cece34bcdb8cc397c50c4959f2b1c9555d
saomarcostecnologia <saomarcostecnologia@gmail.com>
2024-08-22 21:15:05 -0300

    subindo correcoes do codigo

--- index.mjs
+++ index.mjs
@@ -1,24 +1,23 @@
-import { log } from "./log.mjs";
-import { S3 } from '@aws-sdk/client-s3';   
+import { log } from './log.mjs';
+import { S3 } from '@aws-sdk/client-s3';
 
-const s3Client = new S3({region: 'sa-east-1'});
-
-export const handler = async (event, context, callback) => {
+const s3Client = new S3({ region: 'us-east-1' });
+export const handler = async(event) => {
     const record = event.Records[0];
     const Bucket = record.s3.bucket.name;
     const Key = record.s3.object.key;
     const getObjectResult = await s3Client.getObject({
         Bucket,
         Key,
-    }).then( getObjectResult => {
-        const mega_byte = 1024 * 1024;
-
-        if (getObjectResult.ContentLength > 1 * mega_byte) {
-          log('Objeto muito grande');
-          callback('Objeto muito grande');
-        }
-    
-        log('Objeto de Tamanho OK');
-        callback('Objeto de Tamanho OK');
     });
+    const mega_byte = 1024 * 1024;
+
+    if (getObjectResult.ContentLength > 1 * mega_byte) {
+        log('Objeto muito grande');
+
+        return 'Objeto muito grande';
+    }
+
+    log('Objeto de tamanho OK');
+    return 'Objeto de tamanho OK';
 };
